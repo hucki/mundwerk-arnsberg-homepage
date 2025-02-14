bef49b18c7e2b36686f245764d5d8ff3e104d3ca
Stefan Huckschlag <stefan.huckschlag@web.de>
2025-02-14 20:50:21 +0100

    feat(layout): enhance darkMode

--- app/components/footer.tsx
+++ app/components/footer.tsx
@@ -1,21 +1,36 @@
+import { TextLogo } from "./logo";
+
 export const Footer = () => {
   return (
-    <footer className="bg-orange-200 dark:bg-orange-950 p-4 grid grid-cols-3">
-      <div className="text-center">
-        Arnsberg:{" "}
-        <a href="tel:+492931787738" className="text-blue-500 hover:underline">
-          02931 / 787738
-        </a>
+    <footer className="bg-background dark:bg-darkNavBackground  p-4 flex flex-col">
+      <div className="grid grid-cols-1 md:grid-cols-3">
+        <div className="text-center">
+          Gerne nehmen wir Ihren Anruf zur Terminvereinbarung entgegen
+        </div>
+        <div className="text-center">
+          Arnsberg:{" "}
+          <a href="tel:+492931787738" className="text-blue-500 hover:underline">
+            02931 / 787738
+          </a>
+        </div>
+        <div className="text-center">
+          Neheim:{" "}
+          <a href="tel:+492932898061" className="text-blue-500 hover:underline">
+            02932 / 898061
+          </a>
+        </div>
       </div>
-      <div className="text-center">
-        Gerne nehmen wir Ihren Anruf zur Terminvereinbarung entgegen
-      </div>
-      <div className="text-center">
-        Neheim:{" "}
-        <a href="tel:+492932898061" className="text-blue-500 hover:underline">
-          02932 / 898061
-        </a>
-      </div>
+      <p className="col-span-3 text-center text-sm italic pt-2">
+        <span className="whitespace-nowrap">
+          &copy; {new Date().getFullYear().toString()}{" "}
+          <TextLogo
+            textSize="text-xs"
+            hasSubline={false}
+            className="non-italic"
+          />{" "}
+          Logopädie. Alle Rechte vorbehalten.
+        </span>
+      </p>
     </footer>
   );
 };
--- app/components/header.tsx
+++ app/components/header.tsx
@@ -1,11 +1,9 @@
-import { RiMenu2Fill } from "@remixicon/react";
 import { ContactInfo } from "./contactInfo";
 import { ComboLogo } from "./logo";
 
 export const Header = () => {
   return (
-    <header className="bg-orange-50 dark:bg-orange-950 p-4 grid grid-cols-[1fr_auto] md:grid-cols-[42px_auto_200px] items-center">
-      <RiMenu2Fill className="text-orange-500 dark:text-orange-500" />
+    <header className="bg-background dark:bg-darkBackground p-4 grid grid-cols-[1fr_auto] items-center">
       <ComboLogo />
       <ContactInfo />
     </header>
