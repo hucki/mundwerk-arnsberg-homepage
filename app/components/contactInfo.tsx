3d2e21d9d989077ce7c4f3404f222ac35ffc883c
Stefan Huckschlag <stefan.huckschlag@web.de>
2025-02-14 20:52:59 +0100

    feat(ContactInfo): enhance visibility

--- app/components/contactInfo.tsx
+++ app/components/contactInfo.tsx
@@ -2,13 +2,13 @@ import { RiPhoneFill } from "@remixicon/react";
 
 export const ContactInfo = () => {
   return (
-    <div className="md:justify-items-end ">
-      Terminvereinbarung:
+    <div className="md:justify-items-end">
       <a
         href="tel:+492931787738"
-        className="text-orange-500 hover:underline flex flex-row items-center"
+        className="text-white hover:underline flex flex-row items-center bg-primary px-4 py-1 rounded-lg shadow-md "
       >
-        <RiPhoneFill className="text-orange-500 px-1" /> 02931 / 787738
+        <RiPhoneFill className="h-8 w-8 px-1" />{" "}
+        <span className="hidden md:block">02931 / 787738</span>
       </a>
     </div>
   );
