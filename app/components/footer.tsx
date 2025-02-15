238fddb79072f6e914271283fcb02726e219858b
Stefan Huckschlag <stefan.huckschlag@web.de>
2025-02-15 23:57:05 +0100

    feat(footer): use new components

--- app/components/footer.tsx
+++ app/components/footer.tsx
@@ -1,24 +1,13 @@
+import { FooterContact } from "./footerContact";
+import { LegalLinks } from "./legalLinks";
 import { TextLogo } from "./logo";
 
 export const Footer = () => {
   return (
     <footer className="bg-background dark:bg-darkNavBackground mt-16 p-4 flex flex-col">
       <div className="grid grid-cols-1 md:grid-cols-3">
-        <div className="text-center">
-          Gerne nehmen wir Ihren Anruf zur Terminvereinbarung entgegen
-        </div>
-        <div className="text-center">
-          Arnsberg:{" "}
-          <a href="tel:+492931787738" className="text-blue-500 hover:underline">
-            02931 / 787738
-          </a>
-        </div>
-        <div className="text-center">
-          Neheim:{" "}
-          <a href="tel:+492932898061" className="text-blue-500 hover:underline">
-            02932 / 898061
-          </a>
-        </div>
+        <LegalLinks />
+        <FooterContact />
       </div>
       <p className="col-span-3 text-center text-sm italic pt-2">
         <span className="whitespace-nowrap">
