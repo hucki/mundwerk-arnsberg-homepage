333e22b7d64453a3e12e138d2141bc355bc18cfb
Stefan Huckschlag <stefan.huckschlag@web.de>
2025-02-17 00:40:22 +0100

    feat(root): add favicon

--- app/root.tsx
+++ app/root.tsx
@@ -21,6 +21,84 @@ export const links: Route.LinksFunction = () => [
     rel: "stylesheet",
     href: "https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&display=swap",
   },
+  {
+    rel: "apple-touch-icon-precomposed",
+    sizes: "57x57",
+    href: "/favicons/apple-touch-icon-57x57.png",
+    type: "image/png",
+  },
+  {
+    rel: "apple-touch-icon-precomposed",
+    sizes: "114x114",
+    href: "/favicons/apple-touch-icon-114x114.png",
+    type: "image/png",
+  },
+  {
+    rel: "apple-touch-icon-precomposed",
+    sizes: "72x72",
+    href: "/favicons/apple-touch-icon-72x72.png",
+    type: "image/png",
+  },
+  {
+    rel: "apple-touch-icon-precomposed",
+    sizes: "144x144",
+    href: "/favicons/apple-touch-icon-144x144.png",
+    type: "image/png",
+  },
+  {
+    rel: "apple-touch-icon-precomposed",
+    sizes: "60x60",
+    href: "/favicons/apple-touch-icon-60x60.png",
+    type: "image/png",
+  },
+  {
+    rel: "apple-touch-icon-precomposed",
+    sizes: "120x120",
+    href: "/favicons/apple-touch-icon-120x120.png",
+    type: "image/png",
+  },
+  {
+    rel: "apple-touch-icon-precomposed",
+    sizes: "76x76",
+    href: "/favicons/apple-touch-icon-76x76.png",
+    type: "image/png",
+  },
+  {
+    rel: "apple-touch-icon-precomposed",
+    sizes: "152x152",
+    href: "/favicons/apple-touch-icon-152x152.png",
+    type: "image/png",
+  },
+  {
+    rel: "icon",
+    sizes: "196x196",
+    href: "/favicons/favicon-196x196.png",
+    type: "image/png",
+  },
+  {
+    rel: "icon",
+    sizes: "96x96",
+    href: "/favicons/favicon-96x96.png",
+    type: "image/png",
+  },
+  {
+    rel: "icon",
+    sizes: "32x32",
+    href: "/favicons/favicon-32x32.png",
+    type: "image/png",
+  },
+  {
+    rel: "icon",
+    sizes: "16x16",
+    href: "/favicons/favicon-16x16.png",
+    type: "image/png",
+  },
+  {
+    rel: "icon",
+    sizes: "128x128",
+    href: "/favicons/favicon-128.png",
+    type: "image/png",
+  },
 ];
 
 export function Layout({ children }: { children: React.ReactNode }) {
@@ -29,6 +107,22 @@ export function Layout({ children }: { children: React.ReactNode }) {
       <head>
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta name="application-name" content="Mundwerk Logogpädische Praxis" />
+        <meta name="msapplication-TileColor" content="#FFFFFF" />
+        <meta name="msapplication-TileImage" content="mstile-144x144.png" />
+        <meta name="msapplication-square70x70logo" content="mstile-70x70.png" />
+        <meta
+          name="msapplication-square150x150logo"
+          content="mstile-150x150.png"
+        />
+        <meta
+          name="msapplication-wide310x150logo"
+          content="mstile-310x150.png"
+        />
+        <meta
+          name="msapplication-square310x310logo"
+          content="mstile-310x310.png"
+        />
         <Meta />
         <Links />
       </head>
